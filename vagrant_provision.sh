39bdae269a562a82d4b36fd0cbcb507388dacd95
cgarciaarano <cgarciaarano@gmail.com>
2015-09-30 00:19:45 +0200

    Added prerequisites

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y python-pip python-dev build-essential
+sudo apt-get install -y python-pip python-dev build-essential libblas-dev libblas3gf liblapack-dev liblapack3gf gfortran gfortran-4.6 pkg-config
 sudo pip install scikit-learn nltk scipy numpy
 cd /vagrant/tools
