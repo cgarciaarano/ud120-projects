20354c8f8dab68bba91fa0710a414ddd0024b779
cgarciaarano <cgarciaarano@gmail.com>
2015-10-01 12:38:12 +0200

    Added requirements.txt for pip

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y python-pip python-dev build-essential libblas-dev libblas3gf liblapack-dev liblapack3gf gfortran gfortran-4.6 pkg-config
-sudo pip install scikit-learn nltk scipy numpy
+sudo pip install -r requirements.txt
 cd /vagrant/tools && python startup.py
