bd685818b948895022d74138223085a0f0b723ae
cgarciaarano <cgarciaarano@gmail.com>
2015-10-01 19:29:40 +0200

    First mini project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,34 +1,54 @@
 #!/usr/bin/python
 
-""" 
-    this is the code to accompany the Lesson 1 (Naive Bayes) mini-project 
+"""
+    this is the code to accompany the Lesson 1 (Naive Bayes) mini-project
 
     use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+print 'Training data length:{0}'.format(len(features_train))
+print 'Test data length:{0}'.format(len(features_test))
 
+print 'Test data:{0}'.format(features_test[0:3])
 
+sys.exit(0)
+clf = GaussianNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+prediction = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+t0 = time()
+test_score = clf.score(features_train, labels_train)
+print 'Test data score:{0} Time to process:{1}'.format(test_score, round(time()-t0, 3))
+
+t0 = time()
+score = accuracy_score(prediction, labels_test)
+print 'Prediction score:{0} Time to process:{1}'.format(score, round(time()-t0, 3))
 
 #########################################################
 ### your code goes here ###
 
 
 #########################################################
-
-
--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -11,7 +11,7 @@ from sklearn.feature_selection import SelectPercentile, f_classif
 
 
 def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/email_authors.pkl"):
-    """ 
+    """
         this function takes a pre-made list of email texts (by default word_data.pkl)
         and the corresponding authors (by default email_authors.pkl) and performs
         a number of preprocessing steps:
@@ -38,7 +38,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     words_file_handler.close()
 
     ### test_size is the percentage of events assigned to the test set (remainder go into training)
-    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.3, random_state=42)
 
 
 
@@ -50,7 +50,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
 
 
-    ### feature selection, because text is super high dimensional and 
+    ### feature selection, because text is super high dimensional and
     ### can be really computationally chewy as a result
     selector = SelectPercentile(f_classif, percentile=10)
     selector.fit(features_train_transformed, labels_train)
@@ -60,5 +60,5 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     ### info on the data
     print "no. of Chris training emails:", sum(labels_train)
     print "no. of Sara training emails:", len(labels_train)-sum(labels_train)
-    
+
     return features_train_transformed, features_test_transformed, labels_train, labels_test
