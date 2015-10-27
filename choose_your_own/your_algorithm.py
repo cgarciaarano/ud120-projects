aca666721bbb9be93b0227935f92f6052ba6d0c5
cgarciaarano <cgarciaarano@gmail.com>
2015-10-27 20:29:39 +0100

    Lesson 4 completed

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -8,6 +8,8 @@ import traceback
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
+
+
 ### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
 ### in together--separate them so we can give them different colors in the scatterplot,
 ### and visually identify them
@@ -31,12 +33,51 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+print('Number of train features: {0}'.format(len(features_train)))
+print('Number of test features: {0}'.format(len(features_test)))
+
+exercise = 4
+
+if exercise == 1:
+    clf = RandomForestClassifier(max_depth = None, n_estimators = 10, max_features = 'auto')
+    clf.fit(features_train, labels_train)
+
+    acc = clf.score(features_test, labels_test)
+
+    print("Accuracy of RandomForest: {0}".format(acc))
+
+# Beat accuracy 93.6%
+elif exercise == 2:
+    max_acc = 0
+    for max_depth in range(1,15):
+        for n_estimators in range(1,30):
+            for max_features in range(1,2):
+
+                clf = RandomForestClassifier(max_depth = max_depth, n_estimators = n_estimators, max_features = max_features, criterion = 'gini')
+                clf.fit(features_train, labels_train)
+
+                acc = clf.score(features_test, labels_test)
+                if acc > max_acc:
+                    max_acc = acc
+                    print("Accuracy of RandomForest(max_depth={0}, n_estimators={1}, max_features={2}) : {3}".format(max_depth, n_estimators, max_features,acc))
 
 
+    print("Max Accuracy: {0}".format(max_acc))
 
+if exercise == 4:
+    max_acc = 0
+    for n_estimators in range(1,100):
+        clf = AdaBoostClassifier(n_estimators = n_estimators)
+        clf.fit(features_train, labels_train)
 
+        acc = clf.score(features_test, labels_test)
 
-
+        if acc > max_acc:
+            max_acc = acc
+            print("Accuracy of AdaBoostClassifier(n_estimators={0}) : {1}".format(n_estimators, acc))
 
 
 try:
